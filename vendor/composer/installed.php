871c78aa70c5258e1df7ecbefbfbdc2a211dd4fd
Viktoriia Zhuravel <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-06-28 14:41:43 +0200

    adds composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c35f1e5b1a1251ac7c0355adda8138f5634c6e2f',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c35f1e5b1a1251ac7c0355adda8138f5634c6e2f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
